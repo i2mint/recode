246e779c214b7cc36aa2b502bcc404dd370cc310
Owen Lloyd <33433568+owen7lloyd@users.noreply.github.com>
2021-06-18 16:41:22 -0400

    Doctests + assertions

--- recode/__init__.py
+++ recode/__init__.py
@@ -36,8 +36,8 @@ first_element = itemgetter(0)  # "equivalent" to lambda x: x[0]
 @dataclass
 class ChunkedDecoder(Decoder):
     chk_to_frame: ChunkToFrame
-    n_channels: int = None
-    chk_size_bytes: int = None
+    n_channels: int
+    chk_size_bytes: int
 
     # ByteChunker
     def chunker(self, b: bytes) -> Chunks:
@@ -51,32 +51,40 @@ class ChunkedDecoder(Decoder):
         return frames
 
 
-ChkFormat = str  # a str recognized as a struct format
-
-
 def _split_chk_format(chk_format):
+    """
+    >>> assert _split_chk_format('hq') == ('', 'hq')
+    >>> assert _split_chk_format('@hq') == ('@', 'hq')
+    """
     if chk_format[0] in '@=<>!':
         return chk_format[0], chk_format[1:]
     return '', chk_format
 
 
 def _format_chars_part_of_chk_format(chk_format):
+    """
+    >>> assert _format_chars_part_of_chk_format('!hh') == 'hh'
+    >>> _format_chars_part_of_chk_format('q')
+    'q'
+    """
     byte_order, format_chars = _split_chk_format(chk_format)
     return format_chars
 
 
 def _chk_format_to_n_channels(chk_format):
     """
-    >>> _chk_format_to_n_channels('hq')
-    2
-    >>> _chk_format_to_n_channels('@hq')
-    2
+    >>> assert _chk_format_to_n_channels('hq') == 2
+    >>> _chk_format_to_n_channels('@hqt')
+    3
     """
     return len(_format_chars_part_of_chk_format(chk_format))
 
 
 def _chk_format_is_for_single_channel(chk_format):
-    """Checks if a chk_format string is meant for a single channel codec"""
+    """
+    >>> assert _chk_format_is_for_single_channel('h') == True
+    >>> assert _chk_format_is_for_single_channel('@hq') == False
+    """
     return _chk_format_to_n_channels(chk_format) == 1
 
 
@@ -90,15 +98,16 @@ class StructCodecSpecs:
         See https://docs.python.org/3/library/struct.html#format-characters
     :param n_channels: Only n_channels = 1 serves a purpose; to indicate that
 
-    >>> specs = StructCodecSpecs(
-    ...     chk_format='h',
-    ... )
+    To utilise recode, first define your codec specs. If your frame is only one channel (ex. a list) then your format
+    string will include two characters, a byte character (@, =, <, >, !) and a format character. The format character
+    should match the data type of the samples in the frame so they are properly encoded/decoded. This can be seen in the
+    following example.
+
+    >>> specs = StructCodecSpecs(chk_format='h')
     >>> print(specs)
     StructCodecSpecs(chk_format='h', n_channels=1, chk_size_bytes=2)
     >>>
-    >>> encoder = ChunkedEncoder(
-    ...     frame_to_chk=specs.frame_to_chk
-    ... )
+    >>> encoder = ChunkedEncoder(frame_to_chk=specs.frame_to_chk)
     >>> decoder = ChunkedDecoder(
     ...     chk_size_bytes=specs.chk_size_bytes,
     ...     chk_to_frame=specs.chk_to_frame,
@@ -111,14 +120,13 @@ class StructCodecSpecs:
     >>> decoded_frames = list(decoder(b))
     >>> assert decoded_frames == frames
 
+    If your data has more than one channel, then there are two options. If all of the samples are of the same data type,
+    say integer, then your format string needs only one format character and then you can specify the number of channels
+    in your data with the n_channels argument. This can be seen in the following example.
 
-    >>> specs = StructCodecSpecs(
-    ...     chk_format='@h',
-    ...     n_channels = 2
-    ... )
+    >>> specs = StructCodecSpecs(chk_format='@h', n_channels=2)
     >>> print(specs)
     StructCodecSpecs(chk_format='@hh', n_channels=2, chk_size_bytes=4)
-    >>>
     >>>
     >>> encoder = ChunkedEncoder(
     ...     frame_to_chk=specs.frame_to_chk
@@ -135,8 +143,27 @@ class StructCodecSpecs:
     >>> assert b == b'\x01\x00\x02\x00\x03\x00\x04\x00\x05\x00\x06\x00'
     >>> decoded_frames = list(decoder(b))
     >>> assert decoded_frames == frames
-    """
 
+    On the other hand, if each channel has a different data type, say (int, float, int), then your format string needs
+    a format character for each of your channels. This can be seen in the following example, which also shows the use
+    of a different byte character (=).
+
+    >>> specs = StructCodecSpecs(chk_format = '=hdh')
+    >>> print(specs)
+    StructCodecSpecs(chk_format='=hdh', n_channels=3, chk_size_bytes=12)
+    >>>
+    >>> encoder = ChunkedEncoder(frame_to_chk = specs.frame_to_chk)
+    >>> decoder = ChunkedDecoder(
+    ...     chk_size_bytes = specs.chk_size_bytes,
+    ...     chk_to_frame = specs.chk_to_frame,
+    ...     n_channels = specs.n_channels
+    ... )
+    >>> frames = [(1, 2.45, 1), (3, 4.321, 3)]
+    >>> b = encoder(frames)
+    >>> assert b == b'\x01\x00\x9a\x99\x99\x99\x99\x99\x03@\x01\x00\x03\x00b\x10X9\xb4H\x11@\x03\x00'
+    >>> decoded_frames = list(decoder(b))
+    >>> assert decoded_frames == frames
+    """
     chk_format: str
     n_channels: int = None
     chk_size_bytes: int = None
@@ -163,8 +190,16 @@ class StructCodecSpecs:
 
     def frame_to_chk(self, frame):
         if self.n_channels == 1:
+            assert len(frame) == _chk_format_to_n_channels(self.chk_format), (
+                f'The inferred n_channels {_chk_format_to_n_channels(self.chk_format)} did not match the '
+                f'inferred (from frames) {len(frame)}'
+            )
             return pack(self.chk_format, frame)
         else:
+            assert len(frame) == self.n_channels, (
+                f'The given n_channels {self.n_channels} did not match the '
+                f'inferred (from frames) {len(frame)}'
+            )
             return pack(self.chk_format, *frame)
 
     def chk_to_frame(self, chk):
